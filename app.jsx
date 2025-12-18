8e737deb4784196d8274dc57671e9d890a444b40
xiquitos <victoraugustotlb@gmail.com>
2025-12-18 14:32:29 -0300

    changes 1

--- app.jsx
+++ app.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Footer from './components/Footer';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar.jsx';
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/register.jsx';
+import Footer from './components/Footer.jsx';
 
 const App = () => {
+    const [view, setView] = useState('home');
+
+    const handleNavigate = (newView) => {
+        setView(newView);
+        window.scrollTo(0, 0);
+    };
+
     return (
         <div className="app">
-            <Navbar />
-            <Home />
+            <Navbar onNavigate={handleNavigate} />
+            {view === 'home' && <Home onNavigate={handleNavigate} />}
+            {view === 'login' && <Login onNavigate={handleNavigate} />}
+            {view === 'register' && <Register onNavigate={handleNavigate} />}
             <Footer />
         </div>
     );
